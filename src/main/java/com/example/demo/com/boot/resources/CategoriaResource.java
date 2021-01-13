72a8512e839b50c2af4d63aeb5b37b4fe2320c42
Robério Teles <telesdebrito@gmail.com>
2021-01-13 17:50:30 -0300

    Testando a primeira classe de domínio

--- src/main/java/com/example/demo/com/boot/resources/CategoriaResource.java
+++ src/main/java/com/example/demo/com/boot/resources/CategoriaResource.java
@@ -1,18 +1,31 @@
 package com.example.demo.com.boot.resources;
 
 import java.net.Authenticator.RequestorType;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.com.boot.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Funcionou";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(cat2);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
